f649c6e187b15c33d3d5caa88bfbcfd731a5bb77
Connie Gao <connieg@mit.edu>
2015-07-16 17:17:59 -0400

    Added __version__ attribute to pydqed module

--- pydqed.pyx
+++ pydqed.pyx
@@ -45,6 +45,7 @@ import numpy
 cimport numpy
 cimport cython
 
+__version__ = '1.0.0'
 ################################################################################
 
 # Expose the (double-precision) DQED function prototype to this module
