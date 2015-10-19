01ca926d91f9328f9d9c100c13ff9befcc1edc72
Connie Gao <connieg@mit.edu>
2015-10-19 15:14:53 -0400

    Get conda build version from module as well

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ export F77=${PREFIX}/bin/gfortran
 export F90=${PREFIX}/bin/gfortran
 make
 $PYTHON setup.py install
+
+# Save version number stored in pydqed.pyx
+$PYTHON -c 'from pydqed import __version__; print __version__' > ${SRC_DIR}/__conda_version__.txt
+
