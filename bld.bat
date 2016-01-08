e617821d127943e09fbe4d40c7bae860f6c6420b
Connie Gao <connieg@mit.edu>
2016-01-08 15:12:04 -0500

    Fixes to the windows bld.bat file for conda

--- bld.bat
+++ bld.bat
@@ -1,5 +1,11 @@
-cp %PREFIX%\..\..\libs\libpython27.a %PREFIX%\libs
+cp "%PREFIX%\..\..\libs\libpython27.a" "%PREFIX%\libs"
+cp "%PREFIX%\..\..\Lib\distutils\distutils.cfg" "%PREFIX%\Lib\distutils\distutils.cfg
 
-mingw32-make
-%PYTHON% setup.py build_ext --compiler=mingw32 --inplace 
-::install
+gfortran -fPIC -O3 -c dqed.f90 -o dqed.o
+ar rcs libdqed.a dqed.o
+
+"%PYTHON%" setup.py build_ext --build-lib . --build-temp build --pyrex-c-in-temp --compiler=mingw32
+
+"%PYTHON%" setup.py install
+
+
